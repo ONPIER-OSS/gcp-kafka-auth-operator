7b086015de3ff14e4b4acb3688e7435d9d1eb39c
mstiesto <mstiesto01@gmail.com>
2025-01-27 10:22:42 +0100

    DO-1242 test: Add topic-access e2e test

--- example/test-topic-view/main.go
+++ example/test-topic-view/main.go
@@ -13,21 +13,20 @@ func main() {
 	region := os.Getenv("GCP_REGION")
 	kafkaClusterName := os.Getenv("KAFKA_CLUSTER_NAME")
 	kafkaTopic := os.Getenv("KAFKA_TOPIC")
-	bootstrapServer := 	fmt.Sprintf("bootstrap.%s.%s.managedkafka.%s.cloud.goog:9092", kafkaClusterName, region, projectID)
+	bootstrapServer := fmt.Sprintf("bootstrap.%s.%s.managedkafka.%s.cloud.goog:9092", kafkaClusterName, region, projectID)
 	consumerGroupName := fmt.Sprintf("e2e-test-%s-consumer", kafkaTopic)
 
-
 	config := &kafka.ConfigMap{
-		"bootstrap.servers":                       bootstrapServer,
-		"group.id":                                consumerGroupName,
-		"enable.auto.offset.store":                false,
-		"session.timeout.ms":                      6000,
-		"security.protocol":                       "SASL_SSL",
-		"sasl.mechanisms":                         "OAUTHBEARER",
-		"sasl.oauthbearer.token.endpoint.url":     "localhost:14293",
-		"sasl.oauthbearer.client.id":              "unused",
-		"sasl.oauthbearer.client.secret":          "unused",
-		"sasl.oauthbearer.method":                 "oidc",
+		"bootstrap.servers":                   bootstrapServer,
+		"group.id":                            consumerGroupName,
+		"enable.auto.offset.store":            false,
+		"session.timeout.ms":                  6000,
+		"security.protocol":                   "SASL_SSL",
+		"sasl.mechanisms":                     "OAUTHBEARER",
+		"sasl.oauthbearer.token.endpoint.url": "localhost:14293",
+		"sasl.oauthbearer.client.id":          "unused",
+		"sasl.oauthbearer.client.secret":      "unused",
+		"sasl.oauthbearer.method":             "oidc",
 	}
 
 	consumer, err := kafka.NewConsumer(config)
@@ -35,8 +34,6 @@ func main() {
 		log.Fatalf("Failed to create consumer: %v", err)
 	}
 	defer consumer.Close()
-
-
 
 	// Assign partitions and seek to the earliest offset
 	partitions := []kafka.TopicPartition{
@@ -48,17 +45,14 @@ func main() {
 		log.Fatalf("Failed to assign partitions: %v", err)
 	}
 
-	fmt.Println("Waiting for the first message...")
+	log.Println("Waiting for the first message...")
 
 	for {
 		msg, err := consumer.ReadMessage(-1)
 		if err != nil {
 			log.Printf("Consumer error: %v\n", err)
 		} else {
-			fmt.Printf("Consumed message: %s\n", string(msg.Value))
+			log.Printf("Consumed message: %s\n", string(msg.Value))
 		}
 	}
 }
-
-
-
